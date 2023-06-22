79f902a1ee68708d143d3671c7776968694ed074
Todd H. Albert <todd@bocacode.com>
2023-06-22 16:14:29 -0400

    added update

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ import { onRequest } from "firebase-functions/v2/https";
 import logger from "firebase-functions/logger";
 import express from "express";
 import cors from "cors";
-import { getAllCandy, addNewCandy } from "./src/candy.js";
+import { getAllCandy, addNewCandy, updateCandyById } from "./src/candy.js";
 
 const app = express();
 app.use(cors());
@@ -15,5 +15,6 @@ app.get("/test", (req, res) => {
 
 app.get("/candy", getAllCandy);
 app.post("/candy", addNewCandy);
+app.patch("/candy/:candyId", updateCandyById);
 
 export const api = onRequest(app);
